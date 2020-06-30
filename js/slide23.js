4261c7991c84990d94a26b8a9161a369c4f1f030
PhillipKinney <phillipjkinney@gmail.com>
2020-06-30 17:04:44 -0400

    slide 23

---
+++ js/slide23.js
@@ -0,0 +1,10 @@
+
+prompt(userIput1=prompt("Lower Limit?"), userInput2=prompt("Upper Limit?"));
+
+let lowerNumber = userIput1;
+let  upperNumber = userInput2; 
+while(lowerNumber < upperNumber) {
+    document.write(lowerNumber);
+    lowerNumber++;
+}
+
