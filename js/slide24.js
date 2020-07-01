19babb3f54d1e25c25d3bc27e54601bf7b78913c
PhillipKinney <phillipjkinney@gmail.com>
2020-06-30 20:11:02 -0400

    update slide 24

--- js/slide24.js
+++ js/slide24.js
@@ -8,8 +8,6 @@ for(let addNumber=0; addNumber < myArray.length; addNumber++){
    
 }
 
-//console.log(myArray)
-
 
 do{
     prompt(cellIndex=prompt("Enter Index"), newValue=prompt("Enter New Value"));
@@ -19,4 +17,4 @@ while(cellIndex <=0 || cellIndex > 10);
     myArray[cellIndex]=[newValue]
 
 
-   document.write(myArray)
+   console.log(myArray)
